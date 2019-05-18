6f5e58e785822aa5d1961a669c98b825be2c7372
DiegoEstevamDaRosa <diego.rosa8@unisul.br>
2019-05-18 00:51:51 -0300

    SABADO 00:52 DA MADRUGADA - INICIO DO EXERCICIO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { EstadosModule } from './estados/estados.module';
+import { EstadosCadastroComponent } from './estados/estados-cadastro/estados-cadastro.component';
 import { CategoriasCadastroComponent } from './categorias/categorias-cadastro/categorias-cadastro.component';
 import { CategoriasPesquisaComponent } from './categorias/categorias-pesquisa/categorias-pesquisa.component';
 import { CategoriasModule } from './categorias/categorias.module';
@@ -10,12 +12,18 @@ import { AppComponent } from './app.component';
 import { ConfirmationService } from 'primeng/api';
 
 import {Routes, RouterModule} from '@angular/router';
+import { EstadosPesquisaComponent } from './estados/estados-pesquisa/estados-pesquisa.component';
 
 const rotas: Routes = [
   {path: 'categorias', component: CategoriasPesquisaComponent},
   {path: 'categorias/novo', component: CategoriasCadastroComponent},
-  {path: 'categorias/:id', component: CategoriasCadastroComponent}
+  {path: 'categorias/:id', component: CategoriasCadastroComponent},
+
+  {path: 'estados', component: EstadosPesquisaComponent},
+  {path: 'estados/novo', component: EstadosCadastroComponent},
+  {path: 'estados/:id', component: EstadosCadastroComponent},
 ];
+
 
 @NgModule({
   declarations: [
@@ -25,6 +33,7 @@ const rotas: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     CategoriasModule,
+    EstadosModule,
     HttpClientModule,
     RouterModule.forRoot(rotas)
   ],
